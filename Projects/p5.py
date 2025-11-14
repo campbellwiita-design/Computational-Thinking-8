dfe16d4ab3d5c3f1604fdde8ef4e81a232933187
campbellwiita-design <campbellwiita@seattleacademy.org>
2025-11-14 23:28:59 +0000

    alana and Campbell submission

--- Projects/p5.py
+++ Projects/p5.py
@@ -51,24 +51,66 @@ window.tracer(0)
 # Section 2: Setup
 s1 = create_sprite("fairy_map" , 0,0)
 s2 = create_sprite("tooth_fairy" , 0,0)
+s3 = create_sprite("tooth" ,400,150)
+s3.hideturtle()
+s4 = create_sprite("tooth" ,-840,-530)
+s4.hideturtle()
+s5 = create_sprite("tooth" ,1170,-300)
+s5.hideturtle()
+s6 = create_sprite("tooth" ,-545,575)
+s6.hideturtle()
 points = 0
-
+print("Your stops for tonight are at: the big pond, the blue house, the dock, and the north little bush")
+print("Good luck tooth fairy!")
 # Section 3: Controls
 def move_up():
-    s1.setheading(90)
-    s1.forward(5)
+	s1.setheading(90)
+	s1.forward(5)
+	s3.setheading(90)
+	s3.forward(5)
+	s4.setheading(90)
+	s4.forward(5)
+	s5.setheading(90)
+	s5.forward(5)
+	s6.setheading(90)
+	s6.forward(5)
         
 def move_down():
-    s1.setheading(270)
-    s1.forward(5)
-    
+	s1.setheading(270)
+	s1.forward(5)
+	s3.setheading(270)
+	s3.forward(5)
+	s4.setheading(270)
+	s4.forward(5)
+	s5.setheading(270)
+	s5.forward(5)
+	s6.setheading(270)
+	s6.forward(5)
+
 def move_left():
-    s1.setheading(180)
-    s1.forward(5)
+	s1.setheading(180)
+	s1.forward(5)
+	s3.setheading(180)
+	s3.forward(5)
+	s4.setheading(180)
+	s4.forward(5)
+	s5.setheading(180)
+	s5.forward(5)
+	s6.setheading(180)
+	s6.forward(5)
+	
     
-def move_right():    
-    s1.setheading(0)
-    s1.forward(5)
+def move_right():
+	s1.setheading(0)
+	s1.forward(5)
+	s3.setheading(0)
+	s3.forward(5)
+	s4.setheading(0)
+	s4.forward(5)
+	s5.setheading(0)
+	s5.forward(5)
+	s6.setheading(0)
+	s6.forward(5)
 
 window.onkeypress(move_up, "Down")
 window.onkeypress(move_down, "Up")
@@ -85,16 +127,35 @@ while True:
 	 
     
  	# TODO - code for automatic actions
+	if get_distance(s2,s3) < 50:
+		s3.showturtle()
+		points += 1
 
+	if get_distance(s2,s4) < 50:
+		s4.showturtle()
+		points += 1
 
+	if get_distance(s2,s5) < 50:
+		s5.showturtle()
+		points += 1
 
-
+	if get_distance(s2,s6) < 50:
+		s6.showturtle()
+		points += 1
 
 
 	window.update()
 
-	# if :
-	# 	break
+	if timer==3000:
+		s2.write("Children woke up!",font= ("Arial", 20, "normal"))
+		window.update()
+		time.sleep(3)
+		break
 	
 
 print("Game Over")
+
+if points==4: 
+		s2.write("Good job! You found all the teeth!",font= ("Arial", 20, "normal"))
+		
+		
